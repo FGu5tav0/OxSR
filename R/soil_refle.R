1ee02acddcf8658541ecdcba9caa4ceac9731e5a
Gustavo <gustavofrosi@hotmail.com>
2025-01-13 23:05:29 +0100

    atualização soil color

---
+++ R/soil_refle.R
@@ -0,0 +1,14 @@
+#' Tabela do índice SMP
+#'
+#' Dados do valor do índice SMP e a quantidade de calcário necessária para elevar o pH em 5,5, 6 e 6,5
+#'
+#' @format ## `soil_refle`
+#' Um data.frame com 4288 linha e 52 colunas:
+#' \describe{
+#'   \item{Wavelength_nm}{comprimento de onda}
+#'   \item{xn}{amostra de solo}
+#'
+#'   ...
+#' }
+#' @source "em busca"
+"soil_refle"
